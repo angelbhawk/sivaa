07253089650b27b21b286d165310e2286843fc6f
iNube9 <obad_verdugo22@hotmail.com>
2023-05-01 01:07:25 -0700

    cambios en citas

--- SIVAA/Citas.Designer.cs
+++ SIVAA/Citas.Designer.cs
@@ -35,13 +35,25 @@
             comboBox1 = new ComboBox();
             button1 = new Button();
             label1 = new Label();
+            btnAgregar = new Button();
+            btnEliminar = new Button();
+            btnEditar = new Button();
+            dataGridView1 = new DataGridView();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            panel2.SuspendLayout();
             panel1.SuspendLayout();
             panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // panel2
             // 
             panel2.BackColor = Color.White;
+            panel2.Controls.Add(dataGridView1);
             panel2.Dock = DockStyle.Fill;
             panel2.Location = new Point(0, 60);
             panel2.Name = "panel2";
@@ -62,13 +74,16 @@
             // 
             // panel3
             // 
+            panel3.Controls.Add(btnEditar);
+            panel3.Controls.Add(btnEliminar);
+            panel3.Controls.Add(btnAgregar);
             panel3.Controls.Add(button2);
             panel3.Controls.Add(comboBox1);
             panel3.Controls.Add(button1);
             panel3.Dock = DockStyle.Right;
-            panel3.Location = new Point(569, 0);
+            panel3.Location = new Point(263, 0);
             panel3.Name = "panel3";
-            panel3.Size = new Size(351, 60);
+            panel3.Size = new Size(657, 60);
             panel3.TabIndex = 2;
             panel3.Paint += panel1_Paint;
             // 
@@ -76,7 +91,7 @@
             // 
             button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             button2.Font = new Font("Yu Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(20, 19);
+            button2.Location = new Point(327, 18);
             button2.Name = "button2";
             button2.Size = new Size(82, 23);
             button2.TabIndex = 1;
@@ -90,7 +105,7 @@
             comboBox1.Font = new Font("Yu Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point);
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Todos", "Disponibles", "Vendidos" });
-            comboBox1.Location = new Point(127, 18);
+            comboBox1.Location = new Point(433, 18);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(121, 24);
             comboBox1.TabIndex = 0;
@@ -99,7 +114,7 @@
             // 
             button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             button1.Font = new Font("Yu Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(254, 19);
+            button1.Location = new Point(560, 18);
             button1.Name = "button1";
             button1.Size = new Size(77, 23);
             button1.TabIndex = 0;
@@ -116,6 +131,91 @@
             label1.TabIndex = 0;
             label1.Text = "Citas";
             // 
+            // btnAgregar
+            // 
+            btnAgregar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnAgregar.Font = new Font("Yu Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAgregar.Location = new Point(63, 19);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(82, 23);
+            btnAgregar.TabIndex = 6;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnEliminar.Font = new Font("Yu Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEliminar.Location = new Point(151, 19);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(82, 23);
+            btnEliminar.TabIndex = 9;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnEditar.Font = new Font("Yu Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEditar.Location = new Point(239, 18);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(82, 23);
+            btnEditar.TabIndex = 10;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.BackgroundColor = Color.FromArgb(241, 241, 241);
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column5, Column1, Column2, Column3, Column4 });
+            dataGridView1.GridColor = Color.White;
+            dataGridView1.Location = new Point(10, 11);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(900, 399);
+            dataGridView1.TabIndex = 1;
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "#";
+            Column5.Name = "Column5";
+            Column5.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column1.HeaderText = "Empleado";
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column2.HeaderText = "Proveedor";
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            Column3.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column3.HeaderText = "Fecha";
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            Column4.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column4.HeaderText = "Importe";
+            Column4.Name = "Column4";
+            Column4.ReadOnly = true;
+            // 
             // Citas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -126,9 +226,11 @@
             FormBorderStyle = FormBorderStyle.None;
             Name = "Citas";
             Text = "Citas";
+            panel2.ResumeLayout(false);
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
@@ -141,5 +243,14 @@
         private Button button2;
         private ComboBox comboBox1;
         private Button button1;
+        private Button btnAgregar;
+        private Button btnEliminar;
+        private Button btnEditar;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
     }
 }
